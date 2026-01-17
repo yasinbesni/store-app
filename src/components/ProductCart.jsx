87d5fe8fc566a0be678a28e9b711f8ed645792fc
yasin BESNİ <yasinbesni@gmail.com>
2026-01-17 03:04:38 +0300

    UPDATED

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -1,9 +1,14 @@
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, IconButton, Typography } from "@mui/material";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+
 
 export default function ProductCart({ product }) { 
 
     return (
         <Card >
+            <CardActionArea>
             <CardMedia
                 component="img"
                 height="380"
@@ -22,7 +27,15 @@ export default function ProductCart({ product }) {
                 <Typography variant="body1" color="secondary.dark">
                     {product.price}
                 </Typography>
-            </CardContent>
+                <CardActions sx={{ display: "flex",justifyContent:"space-between"}}>
+                    <IconButton>
+                        <FavoriteBorderIcon/>
+                        <FavoriteIcon />  
+                    </IconButton>
+                    <Button size="small">Add to Cart</Button>
+                </CardActions>
+                </CardContent>
+            </CardActionArea>
         </Card>
     )
 }
